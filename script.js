0015411c64587dfe7c75b9f1183690f37ac99359
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-03-08 23:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-// Create the student object
+
 const student = {
   name: "John",
 };
 
-// Function to get all keys from the object
+
 function getKeys(obj) {
-  return Object.keys(obj);
+  return Object.keys(obj); 
 }
 
-// Correct Testing Usage
-const keys = getKeys(student); // Pass the object to the function
-console.log(keys); // Output: ["name"]
-
-// Additional Example
-const studentWithMoreProps = {
-  name: "John",
-  age: 20,
-  city: "New York",
-};
-
-const moreKeys = getKeys(studentWithMoreProps); // Pass the object to the function
-console.log(moreKeys); // Output: ["name", "age", "city"]
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
